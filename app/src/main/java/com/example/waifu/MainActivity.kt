f33f27ed01d19b2ac229bfc8232a6d296c074b3c
John Praiser <praiserjohn@gmail.com>
2020-03-04 12:01:44 -0500

    New Repo Update. Cleaned up new files. Created project files for future development and linked to github. Ensured that project  has been updated to public so that code reviewers can see it.

--- app/src/main/java/com/example/waifu/MainActivity.kt
+++ app/src/main/java/com/example/waifu/MainActivity.kt
@@ -13,29 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val listView = findViewById<ListView>(R.id.main_listview)
-        val redColor = Color.parseColor("#FF0000")
-        listView.setBackgroundColor(redColor)
-    }
-//        listView.adapter =  CustomAdapter()
     }
 
-//    private class CustomAdapter:BaseAdapter(){
-//        override fun getCount(): Int {
-//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//        }
-//
-//        override fun getItemId(position: Int): Long {
-//            return position.toLong()
-//        }
-//
-//        override fun getItem(position: Int): Any {
-//        return position.toString()
-//        }
-//
-//        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//        }
-//    }
 }
