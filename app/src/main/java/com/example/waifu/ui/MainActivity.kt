d0032e6ebe00ae0322acd5731bb360eeec218edf
doughedd <55254751+doughedd@users.noreply.github.com>
2020-03-26 16:35:51 -0400

    Tried to add Date logic on MainActivity and change login activity layout

--- app/src/main/java/com/example/waifu/ui/MainActivity.kt
+++ app/src/main/java/com/example/waifu/ui/MainActivity.kt
@@ -5,19 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.waifu.R
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.Date;
+import java.util.Locale;
+import java.util.Calendar;
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val monthDatePattern = "MMMM d"
+//        val yearPattern = "yyyy"
+//        tvMonthDay.setText(SimpleDateFormat(monthDatePattern).toString())
+//        tvYear.setText(SimpleDateFormat(yearPattern).toString())
 
     }
 
-    fun goToCreateNewTask(view: View) {
+    fun goToCreateNewTask(view: View)
+    {
         val intent = Intent(this, CreateNewTaskActivity::class.java)
         startActivity(intent)
     }
-
-
 }
